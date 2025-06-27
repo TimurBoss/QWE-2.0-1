e0956cdf8fdcbf473e679d34527f07a621ad10e8
TimurBoss <prostoykent1@gmail.com>
2025-06-28 01:09:58 +0500

    Update config.py

--- QWE 2.0/config.py
+++ QWE 2.0/config.py
@@ -1,7 +1,7 @@
 
 token = ['5001471374:AAEyvp2NfkBqx38USiO7nyKyNoj8DKUJ8NI', '5191986561:AAFPEiRHobDiEQT9iOD8FUz5DJoJExjJ9n8'] #  @qwegamebot , @test
-owner_id = 1887634547
+owner_id = 1771886212
 bot_name = "qwegamebot"
-owner = '<a href="t.me/haeshka_qwe_per/">Хаешка</a>'
+owner = '<a href="t.me/Timur06034/">Тимурка</a>'
 chat = '<a href="t.me/qwechat/">QWE | Игровой чат #1</a>'
 channel = '<a href="t.me/qwechannel/">QWE CHANNEL | Канал</a>'
